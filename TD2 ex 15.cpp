a9e52da80709f6f66f049c0a025a1ec564d10e77
yassine jarradi <146362305+YassineJarradi19@users.noreply.github.com>
2023-10-24 22:07:45 +0100

    Update TD2 ex 15.cpp

--- TD2 ex 15.cpp
+++ TD2 ex 15.cpp
@@ -2,35 +2,35 @@
 
 class Fichier {
 private:
-    char* pointeur; // Pointeur vers l'espace mémoire
+    char* pointeur; // Pointeur vers l'espace mÃ©moire
     int longueur;   // Longueur en nombre d'octets
 
 public:
-    // Constructeur
+    
     Fichier(int taille) {
         longueur = taille;
         pointeur = new char[longueur];
     }
 
-    // Destructeur
+  
     ~Fichier() {
         delete[] pointeur;
     }
 
-    // Méthode pour allouer de l'espace mémoire
+    // MÃ©thode pour allouer de l'espace mÃ©moire
     void Creation(int taille) {
         longueur = taille;
         pointeur = new char[longueur];
     }
 
-    // Méthode pour remplir l'espace mémoire
+   
     void Remplit() {
         for (int i = 0; i < longueur; i++) {
             pointeur[i] = 'X'; // Remplissage arbitraire
         }
     }
 
-    // Méthode pour afficher la zone mémoire
+ 
     void Affiche() {
         for (int i = 0; i < longueur; i++) {
             std::cout << pointeur[i] << " ";
@@ -41,13 +41,13 @@ public:
 
 int main() {
     // Instanciation de la classe Fichier
-    Fichier monFichier(10); // Crée un fichier de 10 octets
+    Fichier monFichier(10); // CrÃ©e un fichier de 10 octets
 
-    // Appel des méthodes
+    // Appel des mÃ©thodes
     monFichier.Remplit(); // Remplit le fichier
     monFichier.Affiche(); // Affiche le contenu du fichier
 
     // Destruction de l'objet
-    // Le destructeur sera automatiquement appelé lors de la sortie du scope
+    // Le destructeur sera automatiquement appelÃ© lors de la sortie du scope
     return 0;
 }
